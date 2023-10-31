2fe0be9943fe8378726fcf8b2a54a256f027d8a2
letihab <letihabib1822@gmail.com>
2023-10-31 14:54:34 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from models import storage
 class BaseModel:
     """class from wich all classes will inherit"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """initializes constructor"""
         if kwargs:
             for key in kwargs:
@@ -19,7 +19,7 @@ class BaseModel:
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f" )
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    setattr(self, key, kwargs[key])
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -35,11 +35,11 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = type(self).__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
      
 
 
