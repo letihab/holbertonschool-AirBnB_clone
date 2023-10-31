c1dc2166a906208c0fd6bf092a1fce1ec0925d74
letihab <letihabib1822@gmail.com>
2023-10-31 15:11:17 +0100

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,11 @@ from models import storage
 class BaseModel:
     """class from wich all classes will inherit"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """initializes constructor"""
-        if kwargs:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    setattr(self, key, kwargs[key])
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".\
@@ -32,11 +20,10 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = type(self).__name__
+        obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
