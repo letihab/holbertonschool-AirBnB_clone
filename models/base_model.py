d2368cd9d04fd4968b1902daedde172abe588f98
letihab <letihabib1822@gmail.com>
2023-10-31 15:29:15 +0100

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,15 @@ from datetime import datetime
 class BaseModel:
     """class from wich all classes will inherit"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initializes constructor"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value,
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
