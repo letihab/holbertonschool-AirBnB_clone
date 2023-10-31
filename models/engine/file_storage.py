fbc8647b4bbd61fb1861c3a6a035929ff3b76987
letihab <letihabib1822@gmail.com>
2023-10-31 22:58:52 +0100

    update filestorage with add serialization and deserialisation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,9 @@ import os
 from models.base_model import BaseModel
 
 
-class_dict = {"BaseModel" : BaseModel}
+class_dict = {
+    "BaseModel" : BaseModel
+}
 
 class FileStorage :
     """class that serializes instances to a JSON file
@@ -20,29 +22,29 @@ class FileStorage :
 
     def new(self, obj):
         """set new obj in __objects dictionary"""
-        if obj.id in type(self.__objects):
-            print("exists")
-            return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         type(self).__objects[key] = obj
 
     def save(self):
         """serializes __objects to the Json file __file_path"""
-        new_dict = []
-        for obj in type(self).__objects.values():
-            new_dict.append(obj.to_dict())
+        new_dict = {}
+        for key, obj in type(self).__objects.items():
+            new_dict[key] = obj.to_dict()
         with open(type(self).__file_path, "w", encoding ='utf-8') as file:
             json.dump(new_dict, file)
 
     def reload(self):
         """deserializes the jsonfile to __objects"""
-        if os.path.exists(type(self).__file_path) is True:
+        if not os.path.exists(type(self).__file_path):
             return
         try:
             with open(type(self).__file_path, "r") as file:
                 new_obj = json.load(file)
                 for key, value in new_obj.items():
-                    obj = self.class_dict[value['__class__']](**value)
+                    class_name = value['__class__']
+                    obj_class = class_dict.get(class_name)
+                    if obj_class:
+                        obj = obj_class(**value)
                     type(self).__objects[key] = obj
         except FileNotFoundError:
             pass
