b0186816a0004cfaedc5b76cf46319345ce45c0d
letihab <letihabib1822@gmail.com>
2023-10-31 21:26:07 +0100

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializes constructor"""
         if kwargs:
+            format_dtime = '%Y-%m-%dT%H:%M:%S.%f'
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        value = datetime.strptime(value,
-                        "%Y-%m-%dT%H:%M:%S.%f")
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], format_dtime)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], format_dtime)
+                else:
                     setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
