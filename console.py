842e0181b37e1661aeac173b4db83faa90e1a97c
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-01 10:44:57 +0100

    task 6: console 0.0.1

--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """definition of console"""
     prompt = "(hbnb) "
@@ -16,15 +17,8 @@ class HBNBCommand(cmd.Cmd):
         """Exit the program"""
         return True
 
-    def do_help(self, arg):
-        """Show help for a specific command or list available commands"""
-        if arg:
-            # If the user provides a command as an argument, display help for that command.
-            super().do_help(arg)
-        else:
-            # If no argument is provided, display a list of available commands.
-            super().do_help(arg=None)
-
+    def emptyline(self):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
