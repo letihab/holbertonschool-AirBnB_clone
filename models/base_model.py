cd9c809c5f66a9f7228fe208e6b68b7f402e00a1
letihab <letihabib1822@gmail.com>
2023-11-01 12:55:12 +0100

    reinitialize BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
-"""class basemodel"""
+"""class Basemodel that defines
+all common attributes/methods for 
+other classes
+"""
 
 
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """class from wich all classes will inherit"""
+    """class from wich all other class will inherit"""
 
-    def __init__(self, *args, **kwargs):
-        """initializes constructor"""
-        if kwargs:
-            format_dtime = '%Y-%m-%dT%H:%M:%S.%f'
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, format_dtime))
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+    def __init__(self):
+        """initialized constructor"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
+        """print the representation string"""
+
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
-
+    
     def save(self):
+        """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
--- models/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""file storage autoinit"""
-
-from models.engine.file_storage import FileStorage
-
-"""variable storage, and instance of filestorage"""
-storage = FileStorage()
-storage.reload()
