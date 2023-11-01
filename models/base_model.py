b0fc58711de15f321ff92b1755d52d617cb10f57
letihab <letihabib1822@gmail.com>
2023-11-01 13:26:50 +0100

    add pycostyle recommandation

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,21 @@ from datetime import datetime
 class BaseModel:
     """class from wich all other class will inherit"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialized constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        format_dtime = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, format_dtime))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """print the representation string"""
@@ -30,6 +40,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary with keys value"""
+
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
