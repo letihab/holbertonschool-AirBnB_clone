1fa9b62d74055cc31b848f54210a41b36993ebd3
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-01 09:47:00 +0100

    Task 6 with explicit help

--- console.py
+++ console.py
@@ -16,6 +16,16 @@ class HBNBCommand(cmd.Cmd):
         """Exit the program"""
         return True
 
+    def do_help(self, arg):
+        """Show help for a specific command or list available commands"""
+        if arg:
+            # If the user provides a command as an argument, display help for that command.
+            super().do_help(arg)
+        else:
+            # If no argument is provided, display a list of available commands.
+            super().do_help(arg=None)
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
