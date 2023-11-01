fe4f540b6944805d93534810e3478f37fb6f554a
letihab <letihabib1822@gmail.com>
2023-11-01 23:37:29 +0100

    create class city, place, user, amenity, city, review and state. update filestorage and console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,8 @@ deserializes JSON file to instances"""
 
 
 import json
+from datetime import datetime
+import os
 
 
 class FileStorage:
@@ -34,6 +36,25 @@ class FileStorage:
         with open(self.__file_path, 'w', encoding='utf_8') as file:
             json.dump(data, file)
 
+    def classes(self):
+        """return the reference of valid class"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """deserialize the json file to __objects"""
         from models.base_model import BaseModel
@@ -45,3 +66,41 @@ class FileStorage:
                     self.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
