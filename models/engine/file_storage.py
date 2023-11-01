36b05aaca05ae14991806876577736288185c7e7
letihab <letihabib1822@gmail.com>
2023-11-01 15:02:41 +0100

    fix some issues

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" that serializes instances to a JSON file and deserializes JSON file to instances"""
+"""
+that serializes instances to a JSON file and
+deserializes JSON file to instances"""
 
 
 import json
@@ -25,28 +27,21 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-       """ serializes __objects to the JSON file (path: __file_path)"""
-       data = {}
-       for key, value in self.__objects.items():
-           data[key] = value.to_dict()
-           with open(self.__file_path, 'w', encoding='utf_8') as file:
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        data = {}
+        for key, value in self.__objects.items():
+            data[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding='utf_8') as file:
             json.dump(data, file)
 
     def reload(self):
         """deserialize the json file to __objects"""
         try:
-          with open(self.__file_path, 'r',  encoding='utf-8') as file:
-            data = json.load(file)
-            for key, value in data.items():
-                class_name, obj_id = key.split('.')
-                from models.base_model import BaseModel
-                self.__objects[key] = BaseModel(**value)
+            with open(self.__file_path, 'r',  encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    from models.base_model import BaseModel
+                    self.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
-            
-
-
-           
-
-
-
